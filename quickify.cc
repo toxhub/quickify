b556805bffdd08703717861afa28f0575093bc4b
Ricky Zhou <0x19951125@gmail.com>
2020-09-13 15:36:51 +0800

    chore: import quiche

--- quickify.cc
+++ quickify.cc
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <quiche.h>
+#include "quiche.h"
 
 int main(int argc, char* argv[]) {
   printf("Hello Quickify! \n");
+  printf("This is Quiche Version: %d \n", QUICHE_PROTOCOL_VERSION);
   return 0;
 }
