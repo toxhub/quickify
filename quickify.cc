f6b5e7c8051f55b8476a5a79e9fa88e3faae24bd
Ricky Zhou <0x19951125@gmail.com>
2020-09-13 21:22:21 +0800

    feat: add libuv

--- quickify.cc
+++ quickify.cc
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "quiche.h"
+#include <quiche.h>
+#include <uv.h>
 
 int main(int argc, char* argv[]) {
   printf("Hello Quickify! \n");
   printf("This is Quiche Version: %d \n", QUICHE_PROTOCOL_VERSION);
   return 0;
+}
+
+static void debug_log(const char *line, void *argp) {
+    fprintf(stderr, "%s\n", line);
 }
