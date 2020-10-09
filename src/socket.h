eb8ca9a2c2b6593ab5f2f15bd6c013508784dfb4
Ricky Zhou <0x19951125@gmail.com>
2020-10-09 23:05:54 +0800

    feat: add socket

---
+++ src/socket.h
@@ -0,0 +1,15 @@
+#ifndef QUICKIFY_SOCKET_H_
+#define QUICKIFY_SOCKET_H_
+
+namespace quickify {
+
+class Socket {
+ public:
+ Socket();
+ ~Socket();
+  void init();
+};
+
+}  // namespace quickify
+
+#endif  // QUICKIFY_SOCKET_H_
